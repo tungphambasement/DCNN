0d173a4667e735acb807a02d5b82c6149024e5d2
tungpd <tungphamdoan2509@gmail.com>
2025-09-03 13:24:53 -0400

    Updated message queue to use TBB concurrent queues

--- include/pipeline/pipeline_communicator.hpp
+++ include/pipeline/pipeline_communicator.hpp
@@ -9,6 +9,7 @@
 #include "message.hpp"
 #include "pipeline_endpoint.hpp"
 #include "task.hpp"
+#include "concurrent_message_map.hpp"
 #include <atomic>
 #include <condition_variable>
 #include <functional>
@@ -29,15 +30,10 @@ public:
 
   virtual ~PipelineCommunicator() {
     std::cout << "Communicator getting destroyed" << std::endl;
-    std::lock_guard<std::mutex> in_lock(in_message_mutex_);
     std::lock_guard<std::mutex> out_lock(out_message_mutex_);
     std::lock_guard<std::mutex> rec_lock(recipients_mutex_);
 
-    // Clear all command type queues
-    for (auto& pair : message_queues_) {
-      std::queue<Message<T>> empty_queue;
-      pair.second.swap(empty_queue);
-    }
+    // Clear all messages using the concurrent map
     message_queues_.clear();
 
     std::queue<Message<T>> empty_out;
@@ -79,10 +75,7 @@ public:
       return;
     }
     
-    {
-      std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-      message_queues_[message.command_type].push(message);
-    }
+    message_queues_.push(message.command_type, message);
 
     if (message_notification_callback_) {
       message_notification_callback_();
@@ -99,64 +92,61 @@ public:
   }
 
   inline Message<T> dequeue_input_message() {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
+    Message<T> message;
 
-    if (!message_queues_[CommandType::FORWARD_TASK].empty()) {
-      Message<T> message = message_queues_[CommandType::FORWARD_TASK].front();
-      message_queues_[CommandType::FORWARD_TASK].pop();
+    // Priority order: FORWARD_TASK, BACKWARD_TASK, UPDATE_PARAMETERS first
+    if (message_queues_.pop(CommandType::FORWARD_TASK, message)) {
       return message;
     }
 
-    if (!message_queues_[CommandType::BACKWARD_TASK].empty()) {
-      Message<T> message = message_queues_[CommandType::BACKWARD_TASK].front();
-      message_queues_[CommandType::BACKWARD_TASK].pop();
+    if (message_queues_.pop(CommandType::BACKWARD_TASK, message)) {
       return message;
     }
 
-    if (!message_queues_[CommandType::UPDATE_PARAMETERS].empty()) {
-      Message<T> message = message_queues_[CommandType::UPDATE_PARAMETERS].front();
-      message_queues_[CommandType::UPDATE_PARAMETERS].pop();
+    if (message_queues_.pop(CommandType::UPDATE_PARAMETERS, message)) {
       return message;
     }
 
-    // Then control messages
-    for (auto& pair : message_queues_) {
-      CommandType cmd_type = pair.first;
-      if (cmd_type != CommandType::FORWARD_TASK && 
-          cmd_type != CommandType::BACKWARD_TASK && 
-          cmd_type != CommandType::UPDATE_PARAMETERS &&
-          cmd_type != CommandType::STATUS_REQUEST &&
-          cmd_type != CommandType::STATUS_RESPONSE &&
-          cmd_type != CommandType::HEALTH_CHECK &&
-          cmd_type != CommandType::MEMORY_REPORT &&
-          cmd_type != CommandType::RESOURCE_REQUEST &&
-          cmd_type != CommandType::QUERY_STAGE_INFO &&
-          cmd_type != CommandType::STAGE_INFO_RESPONSE &&
-          cmd_type != CommandType::PRINT_PROFILING) {
-        if (!pair.second.empty()) {
-          Message<T> message = pair.second.front();
-          pair.second.pop();
-          return message;
-        }
+    // Then try control messages (excluding status messages)
+    std::vector<CommandType> control_types = {
+      CommandType::START,
+      CommandType::TRAIN_MODE,
+      CommandType::EVAL_MODE,
+      CommandType::SHUTDOWN,
+      CommandType::HANDSHAKE_REQUEST,
+      CommandType::HANDSHAKE_RESPONSE,
+      CommandType::READY_SIGNAL,
+      CommandType::CONFIG_RECEIVED,
+      CommandType::PARAMETERS_UPDATED,
+      CommandType::ERROR_REPORT,
+      CommandType::TASK_FAILURE,
+      CommandType::BARRIER_SYNC,
+      CommandType::CHECKPOINT_REQUEST,
+      CommandType::CHECKPOINT_COMPLETE,
+      CommandType::CLEAR_PROFILING
+    };
+
+    for (CommandType cmd_type : control_types) {
+      if (message_queues_.pop(cmd_type, message)) {
+        return message;
       }
     }
 
     // Finally status messages
-    for (auto& pair : message_queues_) {
-      CommandType cmd_type = pair.first;
-      if (cmd_type == CommandType::STATUS_REQUEST ||
-          cmd_type == CommandType::STATUS_RESPONSE ||
-          cmd_type == CommandType::HEALTH_CHECK ||
-          cmd_type == CommandType::MEMORY_REPORT ||
-          cmd_type == CommandType::RESOURCE_REQUEST ||
-          cmd_type == CommandType::QUERY_STAGE_INFO ||
-          cmd_type == CommandType::STAGE_INFO_RESPONSE ||
-          cmd_type == CommandType::PRINT_PROFILING) {
-        if (!pair.second.empty()) {
-          Message<T> message = pair.second.front();
-          pair.second.pop();
-          return message;
-        }
+    std::vector<CommandType> status_types = {
+      CommandType::STATUS_REQUEST,
+      CommandType::STATUS_RESPONSE,
+      CommandType::HEALTH_CHECK,
+      CommandType::MEMORY_REPORT,
+      CommandType::RESOURCE_REQUEST,
+      CommandType::QUERY_STAGE_INFO,
+      CommandType::STAGE_INFO_RESPONSE,
+      CommandType::PRINT_PROFILING
+    };
+
+    for (CommandType cmd_type : status_types) {
+      if (message_queues_.pop(cmd_type, message)) {
+        return message;
       }
     }
 
@@ -165,32 +155,18 @@ public:
 
   inline Message<T>
   dequeue_message_by_type(CommandType target_type) {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-
-    if (message_queues_[target_type].empty()) {
+    Message<T> message;
+    if (!message_queues_.pop(target_type, message)) {
       throw std::runtime_error("No message of specified type available");
     }
-
-    Message<T> message = message_queues_[target_type].front();
-    message_queues_[target_type].pop();
     return message;
   }
 
   inline std::vector<Message<T>> dequeue_all_messages_by_type(CommandType target_type) {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-
-    std::vector<Message<T>> messages;
-    auto& queue = message_queues_[target_type];
-    
-    while (!queue.empty()) {
-      messages.push_back(queue.front());
-      queue.pop();
-    }
-
+    std::vector<Message<T>> messages = message_queues_.pop_all(target_type);
     if (messages.empty()) {
       throw std::runtime_error("No messages of specified type available");
     }
-
     return messages;
   }
 
@@ -207,13 +183,7 @@ public:
   }
 
   inline size_t message_count_by_type(CommandType target_type) const {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-    
-    auto it = message_queues_.find(target_type);
-    if (it != message_queues_.end()) {
-      return it->second.size();
-    }
-    return 0;
+    return message_queues_.size(target_type);
   }
 
   inline size_t status_message_count() const {
@@ -225,30 +195,16 @@ public:
   }
 
   inline bool has_input_message() const {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-    for (const auto& pair : message_queues_) {
-      if (!pair.second.empty()) {
-        return true;
-      }
-    }
-    return false;
+    return message_queues_.has_any_message();
   }
 
   inline bool has_task_message() const {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-    
-    auto forward_it = message_queues_.find(CommandType::FORWARD_TASK);
-    auto backward_it = message_queues_.find(CommandType::BACKWARD_TASK);
-    
-    return (forward_it != message_queues_.end() && !forward_it->second.empty()) ||
-           (backward_it != message_queues_.end() && !backward_it->second.empty());
+    return !message_queues_.empty(CommandType::FORWARD_TASK) ||
+           !message_queues_.empty(CommandType::BACKWARD_TASK);
   }
 
   inline bool has_message_of_type(CommandType target_type) const {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-    
-    auto it = message_queues_.find(target_type);
-    return it != message_queues_.end() && !it->second.empty();
+    return !message_queues_.empty(target_type);
   }
 
   inline bool has_status_message() const {
@@ -258,22 +214,6 @@ public:
   inline bool has_parameter_update_message() const {
     bool has_update = has_message_of_type(CommandType::PARAMETERS_UPDATED);
     return has_update;
-  }
-
-  inline std::vector<Message<T>> get_input_messages() {
-    std::lock_guard<std::mutex> lock(this->in_message_mutex_);
-
-    std::vector<Message<T>> messages;
-
-    for (const auto& pair : message_queues_) {
-      auto temp_queue = pair.second;
-      while (!temp_queue.empty()) {
-        messages.push_back(temp_queue.front());
-        temp_queue.pop();
-      }
-    }
-
-    return messages;
   }
 
   inline bool has_output_message() const {
@@ -287,11 +227,10 @@ public:
   }
 
 protected:
-  std::unordered_map<CommandType, std::queue<Message<T>>> message_queues_;
+  ConcurrentMessageMap<T> message_queues_;
 
   std::queue<Message<T>> out_message_queue_;
 
-  mutable std::mutex in_message_mutex_;
   mutable std::mutex out_message_mutex_;
   mutable std::mutex recipients_mutex_;
 
