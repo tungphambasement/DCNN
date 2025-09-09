9c8b7cf409a78186761edd1e3dba9fbe8b94757e
tungpd <tungphamdoan2509@gmail.com>
2025-09-09 16:50:45 -0400

    Added assembly instructions for dot product to avoid compiler being dumb

---
+++ include/utils/simd_asm.hpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Tung D. Pham
+ *
+ * This software is licensed under the MIT License. See the LICENSE file in the
+ * project root for the full license text.
+ */
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Assembly implementation of SIMD dot product for x86_64
+float simd_dot_product_asm(const float *weights, const float *col_data, size_t kernel_size);
+
+#ifdef __cplusplus
+}
+#endif
