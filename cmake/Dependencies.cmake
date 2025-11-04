612b48964a11d32f8432115a2a6e6345e8479da6
tungpd <tungphamdoan2509@gmail.com>
2025-11-04 18:50:13 -0500

    Added google test and some basic unit tests

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -24,8 +24,18 @@ FetchContent_Declare(
     SOURCE_SUBDIR build/cmake
 )
 
+# Google Test
+FetchContent_Declare(
+    googletest
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.14.0
+)
+
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
 # Make the dependencies available
-FetchContent_MakeAvailable(asio nlohmann_json zstd)
+FetchContent_MakeAvailable(asio nlohmann_json zstd googletest)
 
 # Add global compile definitions for ASIO
 add_compile_definitions(ASIO_STANDALONE)
