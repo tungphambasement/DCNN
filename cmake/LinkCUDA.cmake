4e3657836ae99c63e50866226d7128a059566e18
tungpd <tungphamdoan2509@gmail.com>
2025-11-05 13:08:06 -0500

    auto detect cuda smi

--- cmake/LinkCUDA.cmake
+++ cmake/LinkCUDA.cmake
@@ -1,7 +1,14 @@
 function(link_cuda target_name)
   if(ENABLE_CUDA)
+      # Use the auto-detected architecture or fallback
+      if(DEFINED CUDA_ARCH_NUMBER)
+          set(TARGET_CUDA_ARCH ${CUDA_ARCH_NUMBER})
+      else()
+          set(TARGET_CUDA_ARCH "75") # Fallback
+      endif()
+      
       set_target_properties(${target_name} PROPERTIES 
-          CUDA_ARCHITECTURES "89"
+          CUDA_ARCHITECTURES "${TARGET_CUDA_ARCH}"
           CUDA_STANDARD 17
       )
       target_link_libraries(${target_name} PUBLIC CUDA::cudart)
